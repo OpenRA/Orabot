bd1f69d1e7ed42f9cd1a656e906f6b4cd1c036f2
Pavel Penev <penev92@gmail.com>
2019-02-01 02:03:05 +0200

    Simplified GitHub issue number parsing MessageHandlers and fixed the regex used

--- Orabot/EventHandlers/CustomMessageHandlers/OpenRaGitHubIssueNumberMessageHandler.cs
+++ Orabot/EventHandlers/CustomMessageHandlers/OpenRaGitHubIssueNumberMessageHandler.cs
@@ -2,14 +2,10 @@
 {
 	internal class OpenRaGitHubIssueNumberMessageHandler : BaseGitHubIssueNumberMessageHandler
 	{
-		protected override int IssueNumberStartPosition { get; } = 1;
-
-		protected override string RegexMatchPattern { get; } = "#[0-9]+";
-
-		protected override bool RegexMatchCase { get; } = false;
-
 		protected override string RepositoryOwner { get; } = "OpenRA";
 
 		protected override string RepositoryName { get; } = "OpenRA";
+
+		protected override string[] RegexMatchPatternKeywords { get; } = { string.Empty };
 	}
 }
