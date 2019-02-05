599146c7076ed0c3eb55b179db4f1687074c87c8
Pavel Penev <penev92@gmail.com>
2019-02-05 14:35:48 +0200

    Fixed a typo in the SupportDir command output

--- Orabot/Modules/GeneralModule.cs
+++ Orabot/Modules/GeneralModule.cs
@@ -12,7 +12,7 @@ namespace Orabot.Modules
 							 "The support directory is where the game keeps the saved settings, maps, replays, logs and mod assets.  The default location is as follows, but one can choose to move it to an arbitrary location by passing an Engine.SupportDir argument to the Game.exe\n\n" +
 			                 "Windows:    \\Users\\<Username>\\My Documents\\OpenRA\\\n" +
 			                 "OS X:        /Users/<username>/Library/Application Support/OpenRA/\n" +
-			                 "GNU/Linux:    /home/<username>username/.openra/\n" +
+			                 "GNU/Linux:    /home/<username>/.openra/\n" +
 			                 "```");
 		}
 	}
