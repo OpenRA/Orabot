5e1537421e14b19b8fb2053e2d929a27ae7ca2b4
Pavel Penev <penev92@gmail.com>
2019-02-09 23:30:26 +0200

    Added OpenRa mod SDK GitHub issue number parsing

---
+++ Orabot/EventHandlers/CustomMessageHandlers/NumberParsingMessageHandlers/GitHubIssueNumberMessageHandlers/OpenRaModSdkGitHubIssueNumberMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+
+namespace Orabot.EventHandlers.CustomMessageHandlers.NumberParsingMessageHandlers.GitHubIssueNumberMessageHandlers
+{
+	internal class OpenRaModSdkGitHubIssueNumberMessageHandler : BaseGitHubIssueNumberMessageHandler
+	{
+		protected override string RepositoryOwner { get; } = "OpenRA";
+
+		protected override string RepositoryName { get; } = "OpenRAModSDK";
+
+		protected override string[] RegexMatchPatternKeywords { get; } = { "sdk" };
+
+		public OpenRaModSdkGitHubIssueNumberMessageHandler(IRestClient restClient) : base(restClient) { }
+	}
+}
