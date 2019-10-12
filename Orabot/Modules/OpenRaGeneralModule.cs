85adb0c7e2f2c4687f6bc5e8e490203b91cb3dae
Pavel Penev <penev92@gmail.com>
2019-10-12 13:39:26 +0300

    Added commands "book" and "academy" to the OpenRAGeneral module to print information about the OpenRA Book and the OpenRA Academy

--- Orabot/Modules/OpenRaGeneralModule.cs
+++ Orabot/Modules/OpenRaGeneralModule.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 
 namespace Orabot.Modules
@@ -15,6 +17,43 @@ namespace Orabot.Modules
 			                 "OS X:        /Users/<username>/Library/Application Support/OpenRA/\n" +
 			                 "GNU/Linux:    /home/<username>/.openra/\n" +
 			                 "```");
+		}
+
+		[Command("academy")]
+		[Summary("Prints information about the OpenRA Academy.")]
+		public async Task Academy()
+		{
+			const string messageLink = "https://discordapp.com/channels/153649279762694144/520193572256088084/520209549274120202";
+
+			if (!(Context.Guild.Channels.FirstOrDefault(x => x.Name == "navigation") is ITextChannel channel))
+			{
+				return;
+			}
+
+			var embedBuilder = new EmbedBuilder
+			{
+				Description = "The OpenRA Academy is a separate Discord server aimed at helping players get better at the game.\n" +
+				              $"Please refer to [the following message]({messageLink}) in {channel.Mention}, which contains a link with an invite to the server."
+			};
+
+			await ReplyAsync(string.Empty, false, embedBuilder.Build());
+		}
+
+		[Command("book")]
+		[Summary("Prints information about the OpenRA Book.")]
+		public async Task Book()
+		{
+			const string bookUrl = "https://www.openra.net/book/";
+			const string bookRepoUrl = "https://github.com/OpenRA/book";
+
+			var embedBuilder = new EmbedBuilder
+			{
+				Description = $"[The OpenRA book]({bookUrl}) aims to teach new players how to play and existing players how to get better.\n" +
+				              "It also covers creating new games on the OpenRA engine and how the engine works under-the-hood.\n" +
+				              $"You can find the official repository for the book [on GitHub]({bookRepoUrl})."
+			};
+
+			await ReplyAsync(string.Empty, false, embedBuilder.Build());
 		}
 	}
 }
